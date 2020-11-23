11cc3a20a299fc3f139b32c80c7e5d8bdeb38d01
mooyg <61348580+mooyg@users.noreply.github.com>
2020-11-23 16:17:20 +0530

    hm hm

--- src/useOnClickOutside.js
+++ src/useOnClickOutside.js
@@ -4,17 +4,10 @@ const useOnClickOutside = (ref, handler) => {
     const listener = (event) => {
       console.log("event", event.target);
       console.log("REF", ref);
-      if (!ref.current || ref.current.contains(event.target)) {
-        return () => {
-          document.removeEventListener("mousedown", listener);
-        };
-      }
-      handler();
     };
-    document.addEventListener("mousedown", listener);
-    return () => {
-      document.removeEventListener("mousedown", listener);
-    };
+    if (ref.current) {
+      ref.current.addEventListener("mousedown", listener);
+    }
   }, [ref, handler]);
 };
 export default useOnClickOutside;
